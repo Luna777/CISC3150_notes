79215c913b28e09b9c2cb1f7fe4fdbe885dc0c23
Luna777 <xinguan777@gmail.com>
2015-09-04 06:53:19 -0400

    Update 0827.java

--- 0827.java
+++ 0827.java
@@ -1,1 +1,35 @@
+8/27
+
+all the data type in java is signed.
+------------
+in c/c++, int x=y=z=1
+but in java, that will make a mistake.
+------------
+boolean bool= 20%2==0; //this is ok, 但最好不要这样写
+    // 这里先计算 (20%2==0) is true, 然后bool is set to true
+-------------
+declear any constant variable, put final in front of it.
+and make sure your constant is all UPPERCASE.
+    ex: final int ABC=5;
+declear a class file, all the first letter should be UPPERCASE.
+    ex: public class MyClassFile
+method/function name is like: myNewFunction()
+variable name is all lowercase.
+-------------
+in command line tool use:
+>javac Blah.java
+>java Blah
+去run一个java程序
+-------------
+ex:
+		
+
+
+
+
+
+
+
+
+
 
